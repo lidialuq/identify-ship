d7a81f037d10bfa489ecd75af226a9e44acb6df3
lidialuq <lidialuquef@gmail.com>
2023-08-27 19:29:13 +0200

    add inference

---
+++ classifier/main_infer.py
@@ -0,0 +1,21 @@
+import os
+import json
+import torch
+from inferernce import ModelInference
+import sys
+sys.path.append('../')
+from settings import PROJECT_ROOT
+
+model_path = os.path.join(PROJECT_ROOT, 'trained_models', 'woven-energy-5', 'model_epoch_100.pth')
+dataset_path = os.path.join(PROJECT_ROOT, 'data', 'images')
+device = torch.device("cuda:0")
+
+inferer = ModelInference(model_path, dataset_path, device)
+results = inferer.infer()
+
+# Save the results to a JSON file
+save_path = os.path.join(PROJECT_ROOT, 'results', 'inference_results.json')
+with open(save_path, 'w') as outfile:
+    json.dump(results, outfile, indent=4)
+
+print(f"Inference results saved to {save_path}")
